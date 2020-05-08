ff0ce124a81d799acbcc96272d9dab19090c9777
why-zy <59255505+why-zy@users.noreply.github.com>
2020-05-08 17:45:44 +0800

    初始化项目

---
+++ src/router/router.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path:'/',
+        redirect:'/florist'
+    },
+    {
+        name:'Florist',
+        path:'/florist',
+        component: ()=> import('@/views/florist/Florist.vue')
+    },
+    {
+        name:'Flower list query',
+        path:'/flower list query',
+        component: ()=> import('@/views/flower list query/Flower list query.vue')
+    },
+    {
+        name:'Flower material',
+        path:'/flower material',
+        component: ()=>import('@/views/flower material/Flower material.vue')
+    },
+    {
+        name:'Flower Shopping Center',
+        path:'flower Shopping Center',
+        component: ()=>import('@/views/flower Shopping Center/Flower Shopping Center.vue')
+    },
+    {
+        name:'Shopping Cart',
+        path:'/shopping Cart',
+        component: ()=>import('@/views/shopping Cart/Shopping Cart.vue')
+    },
+]
+
+const router = new VueRouter({
+  mode: 'history',  // 路由模式
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                //配置别名
+                'assets': '@/assets',
+                'components': '@/components',
+                'views': '@/views',
+            }
+        }
+    }}
